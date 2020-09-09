d201c24c4f706d7eb44ce9064ea2c7ef95fd55f8
Tabatha Roman <romantabatha17@gmail.com>
2020-09-09 22:04:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,21 +3,21 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-
+array.sort
 end
 
 def using_reverse(array)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def using_first(array)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def using_last(array)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def using_size(array)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
